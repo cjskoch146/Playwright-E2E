7f43e70d9e1f584c23f089ff98da238e0c095243
cjskoch146 <cjs.koch@gmail.com>
2022-12-11 21:41:22 +0100

    remove comment

--- tests/page-object/cookies/privacy-banner.ts
+++ tests/page-object/cookies/privacy-banner.ts
@@ -1,13 +1,7 @@
-// create helper function/method to always accept cookies
 import { expect, Locator, Page } from "@playwright/test";
 
 export class PrivacyBanner {
   readonly page: Page;
-  readonly getStartedLink: Locator;
-  readonly privacyPolicyUrl: Locator;
-  readonly gettingStartedHeader: Locator;
-  readonly pomLink: Locator;
-  readonly tocList: Locator;
   readonly acceptCookiesBtn: Locator;
 
   constructor(page: Page) {
@@ -17,7 +11,7 @@ export class PrivacyBanner {
   async acceptCookies() {
     await this.acceptCookiesBtn.waitFor({
       state: "visible",
-      timeout: 5000,
+      timeout: 5000, // generous timeout to potentially avoid some race conditions
     });
     await this.acceptCookiesBtn.click();
     await this.acceptCookiesBtn.waitFor({ state: "hidden" });
